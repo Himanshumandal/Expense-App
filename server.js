95fe6e809f38822d956e4ab3fe445f2d2cb62558
Himanshu Mandal <himanshum214btechcse2025@kccitm.edu.in>
2024-10-18 23:49:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("*",function(req,res){
 const PORT = 8080 || process.env.PORT;
 
 //listen server
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
